19aec568088d84db2e35b84f4a1b5914cd326e9a
nico.van.roijen <nico.van.roijen@ing.com>
2025-05-11 12:55:21 +0200

    use tabs and checkbox

--- static/script.js
+++ static/script.js
@@ -7,40 +7,56 @@ window.onload = async () => {
 };
 
 function buildForm(data, parent, prefix = '') {
-  for (const key in data) {
-    const value = data[key];
-    const id = prefix + key;
-
-    const group = document.createElement('div');
-    group.className = 'col-md-6';
-
-    const label = document.createElement('label');
-    label.innerText = key;
-    label.className = 'form-label';
-
-    let input;
-    if (typeof value === 'object' && value !== null) {
-      const fieldset = document.createElement('fieldset');
-      const legend = document.createElement('legend');
-      legend.innerText = key;
-      legend.className = 'text-info';
-      fieldset.appendChild(legend);
-      buildForm(value, fieldset, id + '.');
-      group.appendChild(fieldset);
-      parent.appendChild(group);
-      continue;
-    } else {
-      input = document.createElement('input');
-      input.className = 'form-control';
-      input.value = value;
-      input.id = id;
+    if (prefix === '' && data.coins && typeof data.coins === 'object') {
+      createCoinTabs(data.coins, parent);
+      delete data.coins;
     }
-
-    group.appendChild(label);
-    group.appendChild(input);
-    parent.appendChild(group);
+  
+    for (const key in data) {
+      const value = data[key];
+      const id = prefix + key;
+  
+      const group = document.createElement('div');
+      group.className = 'col-md-6';
+  
+      const label = document.createElement('label');
+      label.innerText = key;
+      label.className = 'form-label';
+  
+      let input;
+      if (typeof value === 'boolean') {
+        input = document.createElement('input');
+        input.type = 'checkbox';
+        input.checked = value;
+        input.className = 'form-check-input';
+        input.id = id;
+  
+        const checkDiv = document.createElement('div');
+        checkDiv.className = 'form-check';
+        checkDiv.appendChild(input);
+        checkDiv.appendChild(label);
+        group.appendChild(checkDiv);
+      } else if (typeof value === 'object' && value !== null) {
+        const fieldset = document.createElement('fieldset');
+        const legend = document.createElement('legend');
+        legend.innerText = key;
+        legend.className = 'text-info';
+        fieldset.appendChild(legend);
+        buildForm(value, fieldset, id + '.');
+        group.appendChild(fieldset);
+      } else {
+        input = document.createElement('input');
+        input.className = 'form-control';
+        input.value = value;
+        input.id = id;
+  
+        group.appendChild(label);
+        group.appendChild(input);
+      }
+  
+      parent.appendChild(group);
+    }
   }
-}
 
 function collectFormData(data, prefix = '') {
   const result = {};
