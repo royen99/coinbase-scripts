c910de891470abaa8d12c807c392385532ebc9d7
nico.van.roijen <nico.van.roijen@ing.com>
2025-05-13 21:11:36 +0200

    as service

--- monitor/main.py
+++ monitor/main.py
@@ -3,3 +3,7 @@ from monitor_api import router as monitor_router
 
 app = FastAPI()
 app.include_router(monitor_router, prefix="/api")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info", root_path="/")
